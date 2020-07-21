c6ce9581e6b3d371fb99c78a0d81b6ddb3bc2e9a
Navid <navid.re.s@gmail.com>
2020-07-21 11:02:23 -0600

    fix for custom images

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -6,7 +6,7 @@ import copy
 
 
 class DatasetCatalog(object):
-    DATA_DIR = "datasets"
+    DATA_DIR = "/home/navid/projects/my_work/image-kg-remote/fusion_wpkg/Scene-Graph-Benchmark.pytorch/datasets"
     DATASETS = {
         "coco_2017_train": {
             "img_dir": "coco/train2017",
